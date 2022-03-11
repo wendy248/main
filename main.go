14f1cb06497f5928ec93bf17aa0deade8a7093a9
Wendy <wendywibisono21@gmail.com>
2022-03-11 16:25:57 +0700

    Update main.go

--- main.go
+++ main.go
@@ -9,10 +9,25 @@ import (
 func main() {
 	nama := "Wendy"
 	favorit := "kopi"
+	member := "y"
 
+	//function
 	module.Intro()
-	fmt.Println(module.Salam(nama))
 
+	//function return
+	fmt.Println(module.Salam(nama))
+	
+	//function multiple return
 	hargaSebelum, hargaSesudah := module.Recent(favorit)
 	fmt.Println("Harga minuman", favorit, "sebesar :", hargaSebelum, "sehingga perlu membayar (PPN) :", hargaSesudah)
+
+	//anony func
+	anggotaMember := func(member string) bool{
+		if member == "y" || member == "Y"{
+			return true
+		} else {
+			return false
+		}
+	}
+	module.Member(nama, anggotaMember)
 }
