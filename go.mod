7fac495522d27788ce193746e8586db2d43983e5
Wendy <wendywibisono21@gmail.com>
2022-03-13 13:36:01 +0700

    update

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/wendy248/main
 
 go 1.17
-
-require github.com/wendy248/module/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -49,7 +49,8 @@ func main() {
 
 	//function multiple return
 	hargaSebelum, hargaDiskon := module.Recent(favorit, jumlah)
-	fmt.Println("\nHarga minuman", favorit, "sebesar :", hargaSebelum, "dan setelah diskon member (khusus member) :", hargaDiskon)
+	fmt.Println("\nTotal harga minuman", favorit, "sebesar :", hargaSebelum, "dan setelah diskon member (khusus member) :", hargaDiskon)
+	fmt.Printf("%T\n", jumlah)
 
 	//interface struct
 	pjk := module.PPN{Harga: float32(hargaSebelum)}
